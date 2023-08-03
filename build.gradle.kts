80a46ea6783a6bfccc29bf6cc3cba16e5dfb460d
moseoh <azqazq195@gmail.com>
2023-08-04 01:50:49 +0900

    chore: dependency 및 application.yml 설정 - spring boot 관련 dependency - mysql database - redis - redis - lombok - jwt

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,8 +15,33 @@ repositories {
     mavenCentral()
 }
 
+val jwtVersion = "0.11.5"
+
 dependencies {
+    // spring
     implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
+
+    // database
+    implementation("mysql:mysql-connector-java:8.0.32")
+    testImplementation("com.h2database:h2:2.2.220")
+
+    // jwt
+    implementation("io.jsonwebtoken:jjwt-api:$jwtVersion")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:$jwtVersion")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:$jwtVersion")
+
+    // lombok
+    compileOnly("org.projectlombok:lombok:1.18.28")
+    annotationProcessor("org.projectlombok:lombok:1.18.28")
+    testCompileOnly("org.projectlombok:lombok:1.18.28")
+    testAnnotationProcessor("org.projectlombok:lombok:1.18.28")
+
+    // test
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 

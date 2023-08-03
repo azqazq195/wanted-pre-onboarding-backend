3266af955d060763efd5bc758f8306b7c37b2fc1
moseoh <azqazq195@gmail.com>
2023-08-03 20:56:15 +0900

    init

---
+++ src/test/java/com/wanted/preonboding/PreOnbodingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wanted.preonboding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreOnbodingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

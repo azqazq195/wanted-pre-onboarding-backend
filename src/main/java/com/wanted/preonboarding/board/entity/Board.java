1de33f042f3aa6e3a6e19b37bfe5789859493853
moseoh <azqazq195@gmail.com>
2023-08-04 11:25:37 +0900

    feat: Board Entity - entity - repository

---
+++ src/main/java/com/wanted/preonboarding/board/entity/Board.java
@@ -0,0 +1,19 @@
+package com.wanted.preonboarding.board.entity;
+
+import com.wanted.preonboarding._common.domain.BaseAuditingEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class Board extends BaseAuditingEntity {
+    private String title;
+    
+    @Column(columnDefinition = "text")
+    private String content;
+}

9cd3d1d4f5be162dc3b3a96c22ff4ffa961e9bd1
moseoh <azqazq195@gmail.com>
2023-08-04 22:57:25 +0900

    refactor: AuditorAware 추가 - Entity 생성, 수정시 LoginUserId 삽입

--- src/main/java/com/wanted/preonboarding/_common/config/JpaAuditingConfig.java
+++ src/main/java/com/wanted/preonboarding/_common/config/JpaAuditingConfig.java
@@ -1,9 +1,27 @@
 package com.wanted.preonboarding._common.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.ObjectUtils;
+
+import java.util.Optional;
 
 @Configuration
 @EnableJpaAuditing
 public class JpaAuditingConfig {
+    @Bean
+    public AuditorAware<Long> auditorAware() {
+        return () -> {
+            Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+            if (ObjectUtils.isEmpty(auth) || !auth.isAuthenticated()) {
+                return Optional.empty();
+            } else {
+                return Optional.of((Long) auth.getPrincipal());
+            }
+        };
+    }
 }

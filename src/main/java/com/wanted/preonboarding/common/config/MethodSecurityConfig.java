4747861cd535862b365fd40869778199e31498fb
moseoh <azqazq195@gmail.com>
2023-08-04 11:12:19 +0900

    move: 폴더 이동

--- src/main/java/com/wanted/preonboarding/common/config/MethodSecurityConfig.java
+++
@@ -1,9 +0,0 @@
-package com.wanted.preonboarding.common.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-
-@Configuration
-@EnableMethodSecurity(prePostEnabled = true)
-public class MethodSecurityConfig {
-}
